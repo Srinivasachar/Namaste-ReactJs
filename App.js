72ead477dd020a3e15d49d64a9f6119225dd5e8e
Srinivas S <srinivas.s@sap.com>
2023-04-10 00:02:44 +0530

    Episode 2 - Ignite your app

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("h1",{id: "parent"},
              React.createElement("div",{id: "child"},
-             [React.createElement("h1", {}, "H1 Tag"),
+             [React.createElement("h1", {}, "H1 - children of parent"),
              React.createElement("h2", {}, "H2 Tag")]));
 
 
